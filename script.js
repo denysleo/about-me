dcaf8b8ccc29f385619fa2b6192dcaae6854ae29
Denys <denys_leonardo@hotmail.com>
2024-11-26 23:36:43 -0300

    Primeiro commit - Projeto Sobre Mim

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Página carregada com sucesso!");
+
+    const alertButton = document.getElementById("alertButton");
+    alertButton.addEventListener("click", function () {
+        alert("Desenvolvedor com foco em aprendizado contínuo e paixão por tecnologia!");
+    });
+});
